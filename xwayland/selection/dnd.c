c2fc6978f0df7faedb777eefc6fcd1dda7c0f45b
John Lindgren <john@jlindgren.net>
2026-02-03 15:17:38 -0500

    Revert "xwayland/xwm: implement somewhat asynchronous request flushing"
    There are two different connections between the compositor and XWayland:
    the wayland-protocol connection, and the xwm/X11-protocol connection.
    This change resulted in xwm/X11-protocol messages being delayed,
    causing subtle race-type bugs such as (most recently):
    https://github.com/labwc/labwc/issues/3344
    
    This reverts commit 6ada67da9bb0713df4750f8a177294fa2ff4ba71.

--- xwayland/selection/dnd.c
+++ xwayland/selection/dnd.c
@@ -55,7 +55,7 @@ static void xwm_dnd_send_event(struct wlr_xwm *xwm, xcb_atom_t type,
 		XCB_EVENT_MASK_NO_EVENT,
 		&event,
 		sizeof(event));
-	xwm_schedule_flush(xwm);
+	xcb_flush(xwm->xcb_conn);
 }
 
 static void xwm_dnd_send_enter(struct wlr_xwm *xwm) {
